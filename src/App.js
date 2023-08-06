82462e98537210ae3b091800de11d8150f0a1f0c
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-08-06 09:09:44 +0000

    project completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Switch, Route} from 'react-router-dom'
 
 import LoginForm from './components/LoginForm'
 import Home from './components/Home'
+import ProtectedRoute from './components/ProtectedRoute'
 import Products from './components/Products'
 import Cart from './components/Cart'
 import NotFound from './components/NotFound'
@@ -11,9 +12,9 @@ import './App.css'
 const App = () => (
   <Switch>
     <Route exact path="/login" component={LoginForm} />
-    <Route exact path="/" component={Home} />
-    <Route exact path="/products" component={Products} />
-    <Route exact path="/cart" component={Cart} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <ProtectedRoute exact path="/products" component={Products} />
+    <ProtectedRoute exact path="/cart" component={Cart} />
     <Route component={NotFound} />
   </Switch>
 )
